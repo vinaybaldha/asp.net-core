54b67fd12b5aee2080bca7f0ed65c3c36049f146
Baldha Vinay <113293156+vinaybaldha@users.noreply.github.com>
2024-03-05 19:11:28 +0530

    IActionResult added

---
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("book")]
+        public IActionResult Index()
+        {
+            //id should be supplied
+            if (!Request.Query.ContainsKey("id"))
+            {
+                
+                return BadRequest("id is not supplied");
+            }
+
+            //id can't be empty
+            if (String.IsNullOrEmpty(Convert.ToString(Request.Query["id"])))
+            {
+               
+                return BadRequest("id should not be null or empty");
+            }
+
+            int id = Convert.ToInt32(Request.Query["id"]);
+
+            if (id < 1 || id > 1000)
+            {
+                
+                return BadRequest("id should in between 1 to 1000");
+            }
+
+            bool isAuth = Convert.ToBoolean(Request.Query["isAuth"]);
+
+            if (!isAuth)
+            {
+             
+                return Unauthorized("user is unAuthorized");
+            }
+
+            return RedirectToActionPermanent("Index","Store",new {});
+
+
+
+        }
+    }
+}
---
+++ IActionResultExample/IActionResultExample/Controllers/StoreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("store/books")]
+        public IActionResult Index()
+        {
+            return Content("Store have books");
+        }
+    }
+}
