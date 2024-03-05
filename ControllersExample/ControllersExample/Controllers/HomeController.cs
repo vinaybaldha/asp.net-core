c05eba218562966067194d7264a7096f75660c79
Baldha Vinay <113293156+vinaybaldha@users.noreply.github.com>
2024-03-05 17:00:58 +0530

    controller example added

---
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ControllersExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public ContentResult Index()
+        {
+            return Content("<h1>Welcome</h1> <h2>hello from Index</h2>", "text/html");
+        }
+
+        [Route("person")]
+        public JsonResult about()
+        {
+            Person person = new Person();
+            person.Id= Guid.NewGuid();
+            person.Name = "vinay";
+            person.Age = 20;
+            return Json(person);
+        }
+
+        [Route("contect-us/{mobile:regex(^\\d{{10}}$)}")]
+        public string contact()
+        {
+            return "hello from contects";
+        }
+
+        [Route("download")]
+        public VirtualFileResult file()
+        {
+            return File("/img.jpeg", "image/jpeg");
+        }
+
+        [Route("download2")]
+        public PhysicalFileResult file2()
+        {
+            return PhysicalFile(@"E:\programming\aspnetcore\ControllersExample\sample.pdf", "application/pdf");
+        }
+
+        [Route("download3")]
+        public FileContentResult file3()
+        {
+            byte[] file = System.IO.File.ReadAllBytes(@"E:\programming\aspnetcore\ControllersExample\sample.pdf");
+
+            return File(file, "application/pdf");
+        }
+
+    }
+}
---
+++ ControllersExample/ControllersExample/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ControllersExample.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
