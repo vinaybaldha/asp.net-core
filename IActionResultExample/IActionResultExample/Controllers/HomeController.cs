75b6e76297e87f95715c6a885c122e3b8f521424
Baldha Vinay <113293156+vinaybaldha@users.noreply.github.com>
2024-03-06 13:36:45 +0530

    IActionResult updated

--- IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -1,27 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IActionResultExample.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IActionResultExample.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("book")]
-        public IActionResult Index()
-        {
+        [Route("book/{id?}/{isAuth?}")]
+        public IActionResult Index( int? id,[FromQuery] bool isAuth, Book book)
+        { 
+           
             //id should be supplied
-            if (!Request.Query.ContainsKey("id"))
+            if (id == null)
             {
                 
                 return BadRequest("id is not supplied");
             }
 
             //id can't be empty
-            if (String.IsNullOrEmpty(Convert.ToString(Request.Query["id"])))
-            {
+           // if (String.IsNullOrEmpty(Convert.ToString(Request.Query["id"])))
+            //{
                
-                return BadRequest("id should not be null or empty");
-            }
+             //   return BadRequest("id should not be null or empty");
+            //}
 
-            int id = Convert.ToInt32(Request.Query["id"]);
+           // int id = Convert.ToInt32(Request.Query["id"]);
 
             if (id < 1 || id > 1000)
             {
@@ -29,7 +31,7 @@ namespace IActionResultExample.Controllers
                 return BadRequest("id should in between 1 to 1000");
             }
 
-            bool isAuth = Convert.ToBoolean(Request.Query["isAuth"]);
+            //bool isAuth = Convert.ToBoolean(Request.Query["isAuth"]);
 
             if (!isAuth)
             {
@@ -37,9 +39,9 @@ namespace IActionResultExample.Controllers
                 return Unauthorized("user is unAuthorized");
             }
 
-            return RedirectToActionPermanent("Index","Store",new {});
+            // return RedirectToActionPermanent("Index","Store",new {});
 
-
+            return Content($"Book id: {id} and iaAuth: {isAuth} \n book:{book}");
 
         }
     }
---
+++ IActionResultExample/IActionResultExample/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Models
+{
+    public class Book
+    {
+        public int? id { get; set; }
+        public string? author { get; set; }
+
+        public override string ToString()
+        {
+            return $"book author:{author}, book id: {id}";
+        }
+
+    }
+}
